417bba4eb33f6ae1ec951899b94b63fc4b5becdb
Caio-Elcio <caio.eloy@bandtec.com.br>
2021-10-09 16:37:50 -0300

    Validações de Cadastro e Login, função de exibir e ocultar senha (Desenvolvedor e Investidor)

--- Sistema - WEB/js/login_cadastro_desenvolvedor.js
+++ Sistema - WEB/js/login_cadastro_desenvolvedor.js
@@ -13,7 +13,7 @@ btnSignup.addEventListener("click", function () {
 /* FIM SCRIPT ANIMAÇÃO */
 
 /* SCRIPT MÁSCARA INPUT TELEFONE */
-const tel = document.getElementById('tel') // SELETOR DO CAMPO TELEFONE
+const tel = document.getElementById('telefone') // SELETOR DO CAMPO TELEFONE
 
 tel.addEventListener('keypress', (e) => mascaraTelefone(e.target.value)) // DISPARA QUANDO DIGITADO NO CAMPO
 tel.addEventListener('change', (e) => mascaraTelefone(e.target.value)) // DISPARA QUANDO AUTOCOMPLETADO O CAMPO
@@ -40,3 +40,156 @@ function mascaraCPF(i) {
     if (v.length == 11) i.value += "-";
 }
 /* FIM SCRIPT MÁSCARA INPUT CPF */
+
+/* VALIDAÇÃO DE CADASTRO (NOME) */
+function validarNomeDesenvolvedor() {
+    let nome = document.getElementById('nome');
+    if ((nome.value).length < 3) {
+        nome.style = `border-color: red`;
+    } else {
+        nome.style = `border-color: green`;
+    }
+}
+/* FIM VALIDAÇÃO DE CADASTRO (NOME) */
+
+/* VALIDAÇÃO DE CADASTRO (CIDADE) */
+function validarCidadeDesenvolvedor() {
+    let cidade = document.getElementById('cidade');
+    if ((cidade.value).length < 3) {
+        cidade.style = `border-color: red`;
+    } else {
+        cidade.style = `border-color: green`;
+    }
+}
+/* FIM VALIDAÇÃO DE CADASTRO (CIDADE) */
+
+/* VALIDAÇÃO DE CADASTRO (TELEFONE) */
+function validarTelefoneDesenvolvedor() {
+    if ((telefone.value).length == 15) {
+        telefone.style = `border-color: green`;
+    } else {
+        telefone.style = `border-color: red`;
+    }
+}
+/* FIM VALIDAÇÃO DE CADASTRO (TELEFONE) */
+
+/* VALIDAÇÃO DE CADASTRO (SENHA) */
+function validarSenha1() {
+    var senha_1 = senha1.value;
+    if (senha_1.length >= 5) {
+        senha1.style = `border-color: green`;
+    } else {
+        senha1.style = `border-color: red`;
+    }
+}
+/* FIM VALIDAÇÃO DE CADASTRO (SENHA) */
+
+/* VALIDAÇÃO DE CADASTRO (CONFIRMAR SENHA) */
+function validarSenha2() {
+    var senha_2 = senha2.value;
+    if (senha_2 == senha1.value) {
+        senha2.style = `border-color: green`;
+    } else {
+        senha2.style = `border-color: red`;
+    }
+}
+/* FIM VALIDAÇÃO DE CADASTRO (CONFIRMAR SENHA) */
+
+/* VALIDAÇÃO DE CADASTRO (CPF) */
+function validarCPFDesenvolvedor() {
+    if ((cpf.value).length == 14) {
+        cpf.style = `border-color: green`;
+    } else {
+        cpf.style = `border-color: red`;
+    }
+}
+/* FIM VALIDAÇÃO DE CADASTRO (CPF) */
+
+/* VALIDAÇÃO DE CADASTRO (E-MAIL) */
+function validarEmailDesenvolvedor() {
+    var email1 = email.value;
+    if (email1.includes('@') == true && email1.includes('.com') == true) {
+        email.style = `border-color: green`;
+    } else {
+        email.style = `border-color:red`;
+    }
+}
+/* FIM VALIDAÇÃO DE CADASTRO (E-MAIL) */
+
+/* VALIDAÇÃO DE CADASTRO (COMPETÊNCIA) */
+function validarCompetenciaDesenvolvedor() {
+    let competencia = document.getElementById('competencia');
+    if ((competencia.value).length < 5) {
+        competencia.style = `border-color: red`;
+    } else {
+        competencia.style = `border-color: green`;
+    }
+}
+/* FIM VALIDAÇÃO DE CADASTRO (COMPETÊNCIA) */
+
+/* VALIDAÇÃO DE LOGIN (E-MAIL) */
+function validarEmailDesenvolvedorLogin() {
+    var email2 = emailLogin.value;
+    if (email2.includes('@') == true && email2.includes('.com') == true) {
+        emailLogin.style = `border-color: green`;
+    } else {
+        emailLogin.style = 'border-color:red';
+    }
+}
+/* VALIDAÇÃO DE LOGIN (E-MAIL) */
+
+/* VALIDAÇÃO DE LOGIN (SENHA) */
+function validarSenhaLogin() {
+    var senha_3 = senhaLogin.value;
+    if (senha_3.length >= 5) {
+        senhaLogin.style = `border-color: green`;
+    } else {
+        senhaLogin.style = `border-color:red`;
+    }
+}
+/* FIM VALIDAÇÃO DE LOGIN (SENHA) */
+
+/* FUNÇÃO EXIBIR E OCULTAR SENHA (CADASTRO) */
+const password = document.getElementById('senha1');
+const toggle = document.getElementById('toggle');
+
+function showHide() {
+    if (password.type === 'password') {
+        password.setAttribute('type', 'text');
+        toggle.classList.add('hide')
+    } else {
+        password.setAttribute('type', 'password');
+        toggle.classList.remove('hide')
+    }
+}
+/* FIM FUNÇÃO EXIBIR E OCULTAR SENHA (CADASTRO) */
+
+/* FUNÇÃO EXIBIR E OCULTAR, CONFIRMAR SENHA (CADASTRO) */
+const passwordd = document.getElementById('senha2');
+const togglee = document.getElementById('toggle-confirmar');
+
+function showHidee() {
+    if (passwordd.type === 'password') {
+        passwordd.setAttribute('type', 'text');
+        togglee.classList.add('hide')
+    } else {
+        passwordd.setAttribute('type', 'password');
+        togglee.classList.remove('hide')
+    }
+}
+/* FIM FUNÇÃO EXIBIR E OCULTAR, CONFIRMAR SENHA (CADASTRO) */
+
+/* FUNÇÃO EXIBIR E OCULTAR SENHA (LOGIN) */
+const passworddd = document.getElementById('senhaLogin');
+const toggleee = document.getElementById('toggle-login');
+
+function showHideee() {
+    if (passworddd.type === 'password') {
+        passworddd.setAttribute('type', 'text');
+        toggleee.classList.add('hide')
+    } else {
+        passworddd.setAttribute('type', 'password');
+        toggleee.classList.remove('hide')
+    }
+}
+/* FIM FUNÇÃO EXIBIR E OCULTAR SENHA (LOGIN) */
--- Sistema - WEB/js/login_cadastro_investidor.js
+++ Sistema - WEB/js/login_cadastro_investidor.js
@@ -13,7 +13,7 @@ btnSignup.addEventListener("click", function () {
 /* FIM SCRIPT ANIMAÇÃO */
 
 /* SCRIPT MÁSCARA INPUT TELEFONE */
-const tel = document.getElementById('tel') // SELETOR DO CAMPO TELEFONE
+const tel = document.getElementById('telefone') // SELETOR DO CAMPO TELEFONE
 
 tel.addEventListener('keypress', (e) => mascaraTelefone(e.target.value)) // DISPARA QUANDO DIGITADO NO CAMPO
 tel.addEventListener('change', (e) => mascaraTelefone(e.target.value)) // DISPARA QUANDO AUTOCOMPLETADO O CAMPO
@@ -56,3 +56,155 @@ function mascaraCNPJ(i) {
     if (v.length == 15) i.value += "-";
 }
 /* FIM SCRIPT MÁSCARA INPUT CNPJ */
+
+/* VALIDAÇÃO DE CADASTRO (NOME) */
+function validarNomeInvestidor() {
+    let nome = document.getElementById('nome');
+    if ((nome.value).length < 3) {
+        nome.style = `border-color: red`;
+    } else {
+        nome.style = `border-color: green`;
+    }
+}
+/* FIM VALIDAÇÃO DE CADASTRO (NOME) */
+
+/* VALIDAÇÃO DE CADASTRO (CIDADE) */
+function validarCidadeInvestidor() {
+    let cidade = document.getElementById('cidade');
+    if ((cidade.value).length < 3) {
+        cidade.style = `border-color: red`;
+    } else {
+        cidade.style = `border-color: green`;
+    }
+}
+/* FIM VALIDAÇÃO DE CADASTRO (CIDADE) */
+
+/* VALIDAÇÃO DE CADASTRO (TELEFONE) */
+function validarTelefoneInvestidor() {
+    if ((telefone.value).length == 15) {
+        telefone.style = `border-color: green`;
+    } else {
+        telefone.style = `border-color: red`;
+    }
+}
+/* FIM VALIDAÇÃO DE CADASTRO (TELEFONE) */
+
+/* VALIDAÇÃO DE CADASTRO (SENHA) */
+function validarSenha1() {
+    var senha_1 = senha1.value;
+    if (senha_1.length >= 5) {
+        senha1.style = `border-color: green`;
+    } else {
+        senha1.style = `border-color: red`;
+    }
+}
+/* FIM VALIDAÇÃO DE CADASTRO (SENHA) */
+
+/* VALIDAÇÃO DE CADASTRO (CONFIRMAR SENHA) */
+function validarSenha2() {
+    var senha_2 = senha2.value;
+    if (senha_2 == senha1.value) {
+        senha2.style = `border-color: green`;
+    } else {
+        senha2.style = `border-color: red`;
+    }
+}
+/* FIM VALIDAÇÃO DE CADASTRO (CONFIRMAR SENHA) */
+
+/* VALIDAÇÃO DE CADASTRO (CPF) */
+function validarCPFInvestidor() {
+    if ((cpf.value).length == 14) {
+        cpf.style = `border-color: green`;
+    } else {
+        cpf.style = `border-color: red`;
+    }
+}
+/* FIM VALIDAÇÃO DE CADASTRO (CPF) */
+
+/* VALIDAÇÃO DE CADASTRO (CNPJ) */
+function validarCNPJInvestidor() {
+    if ((cnpj.value).length == 18) {
+        cnpj.style = `border-color: green`;
+    } else {
+        cnpj.style = `border-color: red`;
+    }
+}
+/* FIM VALIDAÇÃO DE CADASTRO (CNPJ) */
+
+/* VALIDAÇÃO DE CADASTRO (E-MAIL) */
+function validarEmailInvestidor() {
+    var email1 = email.value;
+    if (email1.includes('@') == true && email1.includes('.com') == true) {
+        email.style = `border-color: green`;
+    } else {
+        email.style = `border-color:red`;
+    }
+}
+/* FIM VALIDAÇÃO DE CADASTRO (E-MAIL) */
+
+/* VALIDAÇÃO DE LOGIN (E-MAIL) */
+function validarEmailInvestidorLogin() {
+    var email2 = emailLogin.value;
+    if (email2.includes('@') == true && email2.includes('.com') == true) {
+        emailLogin.style = `border-color: green`;
+    } else {
+        emailLogin.style = 'border-color:red';
+    }
+}
+/* VALIDAÇÃO DE LOGIN (E-MAIL) */
+
+/* VALIDAÇÃO DE LOGIN (SENHA) */
+function validarSenhaLogin() {
+    var senha_3 = senhaLogin.value;
+    if (senha_3.length >= 5) {
+        senhaLogin.style = `border-color: green`;
+    } else {
+        senhaLogin.style = `border-color:red`;
+    }
+}
+/* FIM VALIDAÇÃO DE LOGIN (SENHA) */
+
+/* FUNÇÃO EXIBIR E OCULTAR SENHA (CADASTRO) */
+const password = document.getElementById('senha1');
+const toggle = document.getElementById('toggle');
+
+function showHide() {
+    if (password.type === 'password') {
+        password.setAttribute('type', 'text');
+        toggle.classList.add('hide')
+    } else {
+        password.setAttribute('type', 'password');
+        toggle.classList.remove('hide')
+    }
+}
+/* FIM FUNÇÃO EXIBIR E OCULTAR SENHA (CADASTRO) */
+
+/* FUNÇÃO EXIBIR E OCULTAR, CONFIRMAR SENHA (CADASTRO) */
+const passwordd = document.getElementById('senha2');
+const togglee = document.getElementById('toggle-confirmar');
+
+function showHidee() {
+    if (passwordd.type === 'password') {
+        passwordd.setAttribute('type', 'text');
+        togglee.classList.add('hide')
+    } else {
+        passwordd.setAttribute('type', 'password');
+        togglee.classList.remove('hide')
+    }
+}
+/* FIM FUNÇÃO EXIBIR E OCULTAR, CONFIRMAR SENHA (CADASTRO) */
+
+/* FUNÇÃO EXIBIR E OCULTAR SENHA (LOGIN) */
+const passworddd = document.getElementById('senhaLogin');
+const toggleee = document.getElementById('toggle-login');
+
+function showHideee() {
+    if (passworddd.type === 'password') {
+        passworddd.setAttribute('type', 'text');
+        toggleee.classList.add('hide')
+    } else {
+        passworddd.setAttribute('type', 'password');
+        toggleee.classList.remove('hide')
+    }
+}
+/* FIM FUNÇÃO EXIBIR E OCULTAR SENHA (LOGIN) */

ca314f1de5da4d8069f7dd2920380f03edb26c38
Oliveira-joao <joao.vsilva@bandtec.com.br>
2021-11-03 19:32:28 -0300

    Remove pasta de Java indevida

--- Sistema/2ª Sprint/ToWork-Spring-boot/src/main/java/br/com/work/ToWork/Spring/boot/repositorio/ToWorkRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.work.ToWork.Spring.boot.repositorio;
-
-import br.com.work.ToWork.Spring.boot.dominio.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ToWorkRepository extends JpaRepository<Usuario,Integer> {
-}

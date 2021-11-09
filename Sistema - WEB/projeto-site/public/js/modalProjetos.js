7efaa44550421e91d91f1eabff676d2c0c42bcf7
lucasgfelix <lucas.felix@bandtec.com.br>
2021-11-09 15:24:14 -0300

    feat: slides projeto

--- Sistema - WEB/projeto-site/public/js/modalProjetos.js
+++ Sistema - WEB/projeto-site/public/js/modalProjetos.js
@@ -24,3 +24,68 @@
           modal.style.display = 'none';
       }
   }
+  //BOTÃO CURTIDA
+  $(document).ready(function() {
+      $('.contentButton').click(function() {
+          $('.contentButton').toggleClass("heartButton-active")
+          $('.textButton').toggleClass("heartButton-active")
+          $('.heartButton').toggleClass("heartButton-active")
+      });
+  });
+
+  //CARROSEL IMAGEM PROJETOS
+  var balls = document.querySelector('.balls');
+  var quant = document.querySelectorAll('.slides .image');
+  var atual = 0;
+  var imagem = document.getElementById('atual');
+  var avancar = document.getElementById('avancar');
+  var voltar = document.getElementById('voltar');
+  var rolar = true;
+  for (let i = 0; i < quant.length; i++) {
+      var div = document.createElement('div');
+      div.id = i;
+      balls.appendChild(div);
+  }
+  document.getElementById('0').classList.add('imgAtual');
+
+  var pos = document.querySelectorAll('.balls div')
+
+  for (let i = 0; i < pos.length; i++) {
+      pos[i].addEventListener('click', () => {
+          atual = pos[i].id
+          slide()
+          rolar = false
+
+      })
+  }
+  voltar.addEventListener('click', () => {
+      atual--
+      slide()
+      rolar = false
+
+  })
+  avancar.addEventListener('click', () => {
+      atual++
+      rolar = false
+      slide();
+  })
+
+  function slide() {
+      if (atual >= quant.length) {
+          atual = 0;
+      } else if (atual < 0) {
+          atual = quant.length - 1;
+      }
+      document.querySelector('.imgAtual').classList.remove('.imgAtual');
+      imagem.style.marginLeft = -900 * atual + 'px';
+      document.getElementById(atual).classList.add('imgAtual');
+  }
+  setInterval(() => {
+      if (rolar) {
+          atual++
+          slide()
+      } else {
+          rolar = true
+      }
+
+  }, 4000)

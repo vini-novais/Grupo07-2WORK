09430003219f987b87e8ade8c3e30f8e66084b1a
Caio-Elcio <caio.eloy@bandtec.com.br>
2021-11-13 13:16:22 -0300

    Atualização ReactJS

--- Sistema - WEB/projeto-site/public/js/modalProjetos.js
+++ Sistema - WEB/projeto-site/public/js/modalProjetos.js
@@ -1,91 +1,94 @@
-  // Get DOM Elements
-  const modal = document.querySelector('#my-modal');
-  const modalBtn = document.querySelector('#modal-btn');
-  const closeBtn = document.querySelector('.close');
+// Get DOM Elements
+const modal = document.querySelector('#my-modal');
+const modalBtn = document.querySelector('#modal-btn');
+const closeBtn = document.querySelector('.close');
 
-  // Events
-  modalBtn.addEventListener('click', openModal);
-  closeBtn.addEventListener('click', closeModal);
-  window.addEventListener('click', outsideClick);
+// Events
+modalBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+window.addEventListener('click', outsideClick);
 
-  // Open
-  function openModal() {
-      modal.style.display = 'block';
-  }
+// Open
+function openModal() {
+    modal.style.display = 'block';
+}
 
-  // Close
-  function closeModal() {
-      modal.style.display = 'none';
-  }
+// Close
+function closeModal() {
+    modal.style.display = 'none';
+}
 
-  // Close If Outside Click
-  function outsideClick(e) {
-      if (e.target == modal) {
-          modal.style.display = 'none';
-      }
-  }
-  //BOTÃO CURTIDA
-  $(document).ready(function() {
-      $('.contentButton').click(function() {
-          $('.contentButton').toggleClass("heartButton-active")
-          $('.textButton').toggleClass("heartButton-active")
-          $('.heartButton').toggleClass("heartButton-active")
-      });
-  });
+// Close If Outside Click
+function outsideClick(e) {
+    if (e.target == modal) {
+        modal.style.display = 'none';
+    }
+}
+// BOTÃO CURTIDA
+$(document).ready(function () {
+    $('.contentButton').click(function () {
+        $('.contentButton').toggleClass("heartButton-active")
+        $('.textButton').toggleClass("heartButton-active")
+        $('.heartButton').toggleClass("heartButton-active")
+    });
+});
 
-  //CARROSEL IMAGEM PROJETOS
-  var balls = document.querySelector('.balls');
-  var quant = document.querySelectorAll('.slides .image');
-  var atual = 0;
-  var imagem = document.getElementById('atual');
-  var avancar = document.getElementById('avancar');
-  var voltar = document.getElementById('voltar');
-  var rolar = true;
-  for (let i = 0; i < quant.length; i++) {
-      var div = document.createElement('div');
-      div.id = i;
-      balls.appendChild(div);
-  }
-  document.getElementById('0').classList.add('imgAtual');
+// CARROSEL IMAGEM PROJETOS
+var balls = document.querySelector('.balls');
+var quant = document.querySelectorAll('.slides .image');
+var atual = 0;
+var imagem = document.getElementById('atual');
+var avancar = document.getElementById('avancar');
+var voltar = document.getElementById('voltar');
+var rolar = true;
 
-  var pos = document.querySelectorAll('.balls div')
+for (let i = 0; i < quant.length; i++) {
+    var div = document.createElement('div');
+    div.id = i;
+    balls.appendChild(div);
+}
+document.getElementById('0').classList.add('imgAtual');
 
-  for (let i = 0; i < pos.length; i++) {
-      pos[i].addEventListener('click', () => {
-          atual = pos[i].id
-          slide()
-          rolar = false
+var pos = document.querySelectorAll('.balls div')
 
-      })
-  }
-  voltar.addEventListener('click', () => {
-      atual--
-      slide()
-      rolar = false
+for (let i = 0; i < pos.length; i++) {
+    pos[i].addEventListener('click', () => {
+        atual = pos[i].id
+        slide()
+        rolar = false
 
-  })
-  avancar.addEventListener('click', () => {
-      atual++
-      rolar = false
-      slide();
-  })
+    })
+}
 
-  function slide() {
-      if (atual >= quant.length) {
-          atual = 0;
-      } else if (atual < 0) {
-          atual = quant.length - 1;
-      }
-      document.querySelector('.imgAtual').classList.remove('.imgAtual');
-      imagem.style.marginLeft = -48.7 * atual + 'vw';
-      document.getElementById(atual).classList.add('imgAtual');
-  }
-  setInterval(() => {
-      if (rolar) {
-          atual++
-          slide()
-      } else {
-          rolar = true
-      }
+voltar.addEventListener('click', () => {
+    atual--
+    slide()
+    rolar = false
 
-  }, 4000)
+})
+
+avancar.addEventListener('click', () => {
+    atual++
+    rolar = false
+    slide();
+})
+
+function slide() {
+    if (atual >= quant.length) {
+        atual = 0;
+    } else if (atual < 0) {
+        atual = quant.length - 1;
+    }
+    document.querySelector('.imgAtual').classList.remove('.imgAtual');
+    imagem.style.marginLeft = -48.7 * atual + 'vw';
+    document.getElementById(atual).classList.add('imgAtual');
+}
+
+setInterval(() => {
+    if (rolar) {
+        atual++
+        slide()
+    } else {
+        rolar = true
+    }
+}, 4000)
